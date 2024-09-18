1bc9df070ff0c7ec5b3502a260df730efee0a720
denilsonpy <contato.denilsonsilva@gmail.com>
2024-09-18 16:45:16 -0300

    fix(notitification): package schema

--- src/models/package.model.js
+++ src/models/package.model.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 const packageSchema = new mongoose.Schema(
-  {},
+  {
+    _id: {
+      type: String, // Change this to `Number` if your IDs are numeric
+      required: true,
+    },
+  },
   { strict: false, versionKey: false }
 );
 
